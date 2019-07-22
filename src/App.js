181aa40801586ebe8f845873df65d24fb91545ed
mustran <ademimusli000@gmail.com>
2019-07-22 21:39:16 +0200

    project finished

--- src/App.js
+++ src/App.js
@@ -8,17 +8,7 @@ const placeholder = `# Front End Libraries Projects - Build a Markdown Previewer
 ## Objective: Build a CodePen.io app that is functionally similar to this: 
 ### Fulfill the below user stories and get all of the tests to pass. Give it your own personal style.
   
-Heres some code, \`<div></div>\`, between 2 backticks.
 
-\`\`\`
-// this is multi-line code:
-
-function anotherExample(firstLine, lastLine) {
-  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
-    return multiLineCode;
-  }
-}
-\`\`\`
   
 **User Story #1:** I can see a \`textarea\` element with a corresponding id="editor".
 
@@ -27,40 +17,51 @@ function anotherExample(firstLine, lastLine) {
 **User Story #3:** When I enter text into the #editor element, the \`#preview\` element is updated as I type to display the content of the textarea.
 
 **User Story #4:** When I enter GitHub flavored markdown into the \`#editor\` element, the text is rendered as HTML in the #preview element as I type (HINT: You don't need to parse Markdown yourself - you can import the Marked library for this: https://cdnjs.com/libraries/marked).
-Or _italic_.
-Or... wait for it... **_both!_**
-And feel free to go crazy ~~crossing stuff out~~.
 
-There's also [https://codepen.io/freeCodeCamp/full/GrZVVO](https://codepen.io/freeCodeCamp/full/GrZVVO), and
-> Block Quotes!!!
+**User Story #5:** When my markdown previewer first loads, the default text in the \`#editor\` field should contain valid markdown that represents at least one of each of the following elements: a header (H1 size), a sub header (H2 size), a link, inline code, a code block, a list item, a blockquote, an image, and bolded text.
 
-And if you want to get really crazy, even tables:
+**User Story #6:** When my markdown previewer first loads, the default markdown in the \`#editor\` field should be rendered as HTML in the \`#preview\` element.
 
-| Currency Unit       |       Amount       |
-|---------------------|:------------------:|
-| Penny               |    $0.01 (PENNY)   |
-| Nickel              |   $0.05 (NICKEL)   |
-| Dime                |     $0.1 (DIME)    |
-| Quarter             | $0.25 (QUARTER)    |
-| Dollar              | $1 (DOLLAR)        |
-| Five Dollars        | $5 (FIVE)          |
-| Ten Dollars         | $10 (TEN)          |
-| Twenty Dollars      | $20 (TWENTY)       |
-| One-hundred Dollars | $100 (ONE HUNDRED) |
+_Optional Bonus_ (you do not need to make this test pass): When I click a link rendered by my markdown previewer, the link is opened up in a new tab (HINT: read the Marked.js docs for this one!).
 
-- And of course there are lists.
-  - Some are bulleted.
-     - With different indentation levels.
-        - That look like this.
+> GitHub repository.
+
+The [link](https://github.com/mustran/Markdown-Previewer-with-ReactJS) to my repo.
+
+Tasks:
+
+| User Story          |Correct/Incorrect|
+|---------------------|:---------------:|
+| User Story #1       |    Completed    |
+| User Story #2       |    Completed    |
+| User Story #3       |    Completed    |
+| User Story #4       |    Completed    |
+| User Story #5       |    Completed    |
+| User Story #6       |    Completed    |
+| Bonus               |    Completed    |
 
 
-1. And there are numbererd lists too.
-1. Use just 1s if you want! 
-1. But the list goes on...
-- Even if you use dashes or asterisks.
-* And last but not least, let's not forget embedded images:
+Heres some code, on how I completed the Bonus task:
 
-![React Logo w/ Text](https://goo.gl/Umyytc)
+\`\`\`
+// this is multi-line code:
+
+let renderer = new marked.Renderer();
+renderer.link = (href, title, text) => \`<a target="_blank" href="\${ href }" title="\${ title }">\${ text }</a>\`;
+\`\`\`
+
+in the \`output\` component I added the renderer
+
+\`\`\`
+<div dangerouslySetInnerHTML={{ __html: marked(props.outputMD, {renderer}) }} />
+\`\`\`
+
+- I created two components.
+  - One for the editor.
+     - One for the preview
+> Screenshot of the project
+
+![Screenshot](https://image.prntscr.com/image/aKUgDhtKSN_0XiLlPZRRkw.png)
 `;
 
 class App extends Component {
