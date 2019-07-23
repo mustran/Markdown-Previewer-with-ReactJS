56ba6dae92860bdc5ceb9a7b8eeb333a01d996a4
mustran <ademimusli000@gmail.com>
2019-07-23 02:01:30 +0200

    added responsivness

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -3,7 +3,7 @@ import styles from './styles/input.module.css';
 
 const Input = props => {
     return (
-        <div className={styles.inputF}>
+        <div className={styles.inputF} id="edit">
             <textarea id="editor" value={props.inputMD} onChange={props.changeHandler} />
         </div>
     );
